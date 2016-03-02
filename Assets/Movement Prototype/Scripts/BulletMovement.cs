d89b03ed455d4b4aad342cd0081ec552ec2ec84a
Johnathon Root <jroot@mit.edu>
2016-03-02 03:46:12 -0500

    Created a prototype movement system with 2 actors piloting a cockpit and 2 guns.

---
+++ Assets/Movement Prototype/Scripts/BulletMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float speed;
+    int life;
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddRelativeForce(new Vector2(0, speed));
+        life = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == gameObject.tag)
+        {
+            Destroy(coll.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        life++;
+        if (life > 200)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
