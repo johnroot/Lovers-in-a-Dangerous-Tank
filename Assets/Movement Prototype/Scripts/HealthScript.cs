6b220afb910606800934f26797b5b1dab8637d51
Denis Li <denisli@mit.edu>
2016-03-06 11:45:28 -0500

    Created a health bar UI.

---
+++ Assets/Movement Prototype/Scripts/HealthScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+    public float maxHealth = 100f;
+    public float health = 100f;
+    public GameObject healthBar;
+
+    public void DecreaseHealth(float damage)
+    {
+        health = System.Math.Max(0, health - damage);
+    }
+
+    public void UpdateHealthBar()
+    {
+        healthBar.transform.localScale = new Vector3(health / maxHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+
+}
