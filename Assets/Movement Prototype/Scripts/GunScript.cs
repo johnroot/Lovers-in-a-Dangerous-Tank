8452d64516037ec732d9fae88122a1e3cd42987c
Kevin Peng <kpeng94@mit.edu>
2016-03-05 21:17:33 -0500

    Adding script for guns in general.

---
+++ Assets/Movement Prototype/Scripts/GunScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class GunScript : MonoBehaviour {
+
+    public float rotationSpeed;
+    public float fireRate;
+    public GameObject turretBullet;
+    public int ammo;
+
+    int timeElapsedSinceFire = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+        timeElapsedSinceFire++;
+	}
+
+    /**
+     * TODO: finish doc
+     * Rotate turret based on the horizontal
+     * @param horizontal - horizontal axis position
+     */
+    void Rotate (float horizontal) {
+        transform.Rotate(0, 0, rotationSpeed * horizontal * Time.deltaTime);
+    }
+
+    void Fire() {
+        if (timeElapsedSinceFire < fireRate) {
+            Instantiate(turretBullet, transform.position, transform.rotation);
+            timeElapsedSinceFire = 0;
+        }
+    }
+}
