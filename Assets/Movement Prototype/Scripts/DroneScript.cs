e7295ea0c5cc92911d5f73c5cd9ad540f5093d8e
Denis Li <denisli@mit.edu>
2016-03-08 15:23:05 -0500

    Created a script for drone movement and collision.

---
+++ Assets/Movement Prototype/Scripts/DroneScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneScript : MonoBehaviour {
+
+    public float damage;
+    public float speed;
+    public GameObject target;
+
+    Vector2 dronePosition;
+    Vector2 targetPosition;
+    Rigidbody2D rb;
+	
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        dronePosition = rb.position;
+        targetPosition = target.transform.gameObject.GetComponent<Rigidbody2D>().position;
+        rb.velocity = speed * (targetPosition - dronePosition).normalized;
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        HealthScript healthScript = coll.gameObject.GetComponent<HealthScript>();
+        if (healthScript != null)
+        {
+            healthScript.DecreaseHealth(damage);
+        }
+        Destroy(gameObject);
+    }
+
+}
