b5017c97a0bd03ff5a250c94af8d3668bd7b6375
Denis Li <denisli@mit.edu>
2016-03-09 16:01:34 -0500

    Drone and drone spawner scripts added. Currently not limiting the number of drones that can be spawned.

---
+++ Assets/Movement Prototype/Scripts/DroneSpawnerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneSpawnerScript : MonoBehaviour {
+
+    public GameObject drone;
+    public int spawnRate;
+    int timeSincePreviousSpawn = 0;
+    
+    public void FixedUpdate()
+    {
+        if (timeSincePreviousSpawn < spawnRate)
+        {
+            timeSincePreviousSpawn++;
+        }
+    }
+
+    public GameObject SpawnDrone()
+    {
+        if (timeSincePreviousSpawn == spawnRate)
+        {
+            GameObject drone = (GameObject) Instantiate(drone, transform.position, transform.rotation);
+            timeSincePreviousSpawn = 0;
+            return drone;
+        }
+        return null;
+    }
+	
+}
