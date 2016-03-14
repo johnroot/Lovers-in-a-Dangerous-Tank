4ad597bfc779d63b6716c0e94bac726c3b152b2d
Johnathon Root <johnathon.root@gmail.com>
2016-03-13 22:20:08 -0400

    Created a start screen. Player 1 can select a stage and start with A. When a player dies, automatically returns to Start after 5 seconds.

---
+++ Assets/Movement Prototype/Scripts/RestartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class RestartGame : MonoBehaviour {
+
+    public float waitTime;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(RestartGameAfter(waitTime));
+	}
+	
+    IEnumerator RestartGameAfter(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(0);
+    }
+}
